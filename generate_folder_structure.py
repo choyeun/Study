492c7ad68bf09e36e0b8b781f60d21e9df29bd8f
choyeun <94655527+choyeun@users.noreply.github.com>
2024-05-27 10:53:37 +0900

    프로젝트: generate_folder_structure.py 업데이트

--- generate_folder_structure.py
+++ generate_folder_structure.py
@@ -1,26 +1,41 @@
 import os
 
-def generate_folder_structure(dir_path, prefix=''):
-    structure = ''
+EXCLUDE_DIRS = {".git", ".github"}
+EXCLUDE_FILES = {".gitignore", "generate_folder_structure.py"}
+
+
+def generate_folder_structure(dir_path, prefix=""):
+    structure = ""
     for entry in sorted(os.listdir(dir_path)):
         full_path = os.path.join(dir_path, entry)
-        if os.path.isdir(full_path):
-            structure += f'{prefix}- {entry}/\n'
-            structure += generate_folder_structure(full_path, prefix + '  ')
-        else:
-            structure += f'{prefix}- {entry}\n'
+        if os.path.isdir(full_path) and entry not in EXCLUDE_DIRS:
+            structure += f"{prefix}- {entry}/\n"
+            structure += generate_folder_structure(full_path, prefix + "  ")
+        elif os.path.isfile(full_path) and entry not in EXCLUDE_FILES:
+            structure += f"{prefix}- {entry}\n"
     return structure
 
+
 def update_readme(structure):
-    readme_path = 'README.md'
-    with open(readme_path, 'r', encoding='utf-8') as f:
+    readme_path = "README.md"
+    with open(readme_path, "r", encoding="utf-8") as f:
         readme_content = f.read()
 
-    new_readme_content = readme_content.split('## 폴더 구조\n\n')[0] + f'## 폴더 구조\n\n{structure}'
+    if "## 폴더 구조" in readme_content:
+        new_readme_content = (
+            readme_content.split("## 폴더 구조\n\n")[0] + f"## 폴더 구조\n\n{structure}"
+        )
+        if "##" in new_readme_content.split("## 폴더 구조\n\n")[1]:
+            new_readme_content += (
+                "\n" + new_readme_content.split("## 폴더 구조\n\n")[1].split("\n", 1)[1]
+            )
+    else:
+        new_readme_content = readme_content + f"\n## 폴더 구조\n\n{structure}"
 
-    with open(readme_path, 'w', encoding='utf-8') as f:
+    with open(readme_path, "w", encoding="utf-8") as f:
         f.write(new_readme_content)
 
+
 if __name__ == "__main__":
-    folder_structure = generate_folder_structure('.')
+    folder_structure = generate_folder_structure(".")
     update_readme(folder_structure)
